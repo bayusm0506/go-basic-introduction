5fb3484ad0825970e59002ab63a36fb8422874c3
Bayu Setra Maulana <bayumaulana472@gmail.com>
2022-12-27 21:47:47 +0700

    Variable declaration not using data type

---
+++ chapter-A.3-variable/3-variable-declaration-not-using-data-type.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var firstName string = "Jhon" // variable write use manifest typing style
+	lastName := "Rambo"           // variable write use type inference
+
+	fmt.Printf("Hello %s %s!\n", firstName, lastName)
+}
