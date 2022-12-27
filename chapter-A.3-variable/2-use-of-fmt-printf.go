18a134a47985f3306d1936f927ea902af81a832a
Bayu Setra Maulana <bayumaulana472@gmail.com>
2022-12-27 21:42:15 +0700

    Use of fmt printf

---
+++ chapter-A.3-variable/2-use-of-fmt-printf.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var firstName string = "Jhon"
+
+	var lastName string
+	lastName = "Rambo"
+
+	fmt.Printf("Hello Jhon Rambo!\n")
+	fmt.Printf("Hello %s %s!\n", firstName, lastName)
+	fmt.Println("Hello", firstName, lastName+"!")
+}
