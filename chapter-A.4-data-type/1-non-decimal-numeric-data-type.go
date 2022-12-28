02d35f8675b5c925a7335a1c1cb4760604a50f90
Bayu Setra Maulana <bayumaulana472@gmail.com>
2022-12-28 21:28:39 +0700

    Non decimal numeric data type

---
+++ chapter-A.4-data-type/1-non-decimal-numeric-data-type.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var positiveNumber uint8 = 30
+	var negativeNumber = -20221228
+
+	fmt.Printf("Positif Number: %d\n", positiveNumber)
+	fmt.Printf("Negative Number: %d\n", negativeNumber)
+}
