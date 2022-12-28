562e7a791a31db80702f71c44496958cbd52e8d6
Bayu Setra Maulana <bayumaulana472@gmail.com>
2022-12-28 21:32:48 +0700

    Decimal numeric data type

---
+++ chapter-A.4-data-type/2-decimal-numeric-data-type.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var decimalNumber = 2020.12
+
+	fmt.Printf("Decimal Number: %f\n", decimalNumber)
+	fmt.Printf("Decimal Number: %.3f\n", decimalNumber)
+}
