d33d70f4d1db566b3d2cb4fd1f44522bc88a0e33
Bayu Setra Maulana <bayumaulana472@gmail.com>
2023-01-02 22:06:32 +0700

    Array

---
+++ chapter-A.9-array/1-array.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var names [4]string
+	names[0] = "Jhon"
+	names[1] = "Doe"
+	names[2] = "Learn"
+	names[3] = "Golang"
+
+	fmt.Println(names[0], names[1], names[2], names[3])
+}
