535e0e2456d1cd3b65066be439d51f15081ae2dd
Bayu Setra Maulana <bayumaulana472@gmail.com>
2023-01-03 16:50:13 +0700

    Array initial

---
+++ chapter-A.9-array/2-array-initial.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var fruits = [4]string{"apple", "grape", "banana", "melon"}
+
+	fmt.Println("Number of element \t\t", len(fruits))
+	fmt.Println("All element \t", fruits)
+}
