f9dabcf91afafd11d47ae8c23a5fabd87ce6e72c
Bayu Setra Maulana <bayumaulana472@gmail.com>
2023-01-06 23:06:39 +0700

    Reference slice data type

---
+++ chapter-A.10-slice/3-reference-data-type-slice.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var fruits = []string{"apple", "grape", "banana", "melon"}
+
+	var aFruits = fruits[0:3]
+	var bFruits = fruits[1:4]
+
+	var aaFruits = aFruits[1:2]
+	var bbFruits = bFruits[0:1]
+
+	fmt.Println(fruits)
+	fmt.Println(aFruits)
+	fmt.Println(bFruits)
+	fmt.Println(aaFruits)
+	fmt.Println(bbFruits)
+
+	// "grape" fruit change to "pineaple"
+	bbFruits[0] = "pineaple"
+
+	fmt.Println(fruits)
+	fmt.Println(aFruits)
+	fmt.Println(bFruits)
+	fmt.Println(aaFruits)
+	fmt.Println(bbFruits)
+}
