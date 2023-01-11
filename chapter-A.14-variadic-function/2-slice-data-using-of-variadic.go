f448a7ef935c9d5e76a24f021ced65826dea3063
Bayu Setra Maulana <bayumaulana472@gmail.com>
2023-01-11 13:23:06 +0700

    Slice data using of variadic

---
+++ chapter-A.14-variadic-function/2-slice-data-using-of-variadic.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numbers = []int{2, 5, 6, 3, 7, 5, 7, 8, 9}
+	var avg = calculate(numbers...)
+	var msg = fmt.Sprintf("Average : %.2f", avg)
+
+	fmt.Println(msg)
+}
+
+func calculate(numbers ...int) float64 {
+	var total int = 0
+	for _, number := range numbers {
+		total += number
+	}
+
+	var avg = float64(total) / float64(len(numbers))
+	return avg
+}
