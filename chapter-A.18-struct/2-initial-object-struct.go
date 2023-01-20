0491b37191d08e1ed51c90190d6448448fc2a2be
Bayu Setra Maulana <bayumaulana472@gmail.com>
2023-01-20 07:38:46 +0700

    Initial object struct

---
+++ chapter-A.18-struct/2-initial-object-struct.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type student struct {
+	name  string
+	grade int
+}
+
+func main() {
+	var s1 = student{}
+	s1.name = "Jhon"
+	s1.grade = 3
+
+	var s2 = student{"Doe", 3}
+	var s3 = student{s1.name, 2}
+
+	fmt.Println("Student 1 : ", s1.name)
+	fmt.Println("Student 2 : ", s2.name)
+	fmt.Println("Student 3 : ", s3.name)
+}
