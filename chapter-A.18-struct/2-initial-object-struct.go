546effb1883b8d54ae8452978030ccb703fd1873
Bayu Setra Maulana <bayumaulana472@gmail.com>
2023-01-20 07:40:53 +0700

    Initial object struct

--- chapter-A.18-struct/2-initial-object-struct.go
+++ chapter-A.18-struct/2-initial-object-struct.go
@@ -14,8 +14,10 @@ func main() {
 
 	var s2 = student{"Doe", 3}
 	var s3 = student{s1.name, 2}
+	var s4 = student{name: "Rambo"}
 
 	fmt.Println("Student 1 : ", s1.name)
 	fmt.Println("Student 2 : ", s2.name)
 	fmt.Println("Student 3 : ", s3.name)
+	fmt.Println("Student 4 : ", s4.name)
 }
